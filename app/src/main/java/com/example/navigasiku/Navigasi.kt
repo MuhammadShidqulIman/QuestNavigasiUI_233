babeea386fab39f70b6a3870c22d8c8812ed2c1d
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2025-11-06 09:14:13 +0700

    tambah Scaffold untuk navigasi

--- app/src/main/java/com/example/navigasiku/Navigasi.kt
+++ app/src/main/java/com/example/navigasiku/Navigasi.kt
@@ -10,3 +10,10 @@ fun DataApp(
     navController: NavHostController = rememberNavController(),
     modifier: Modifier
 ){
+
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+            modifier = Modifier.padding(paddingValues = isiRuang)
+        ){
